8c97c65104724d14eb85393c1f5c1a9fc54afd25
Dmitriy Drobotov <dmitriy.drobotov@studiomobile.ru>
2019-03-17 15:12:21 +0300

    Use library interfaces in AbstractPerson

--- Implementation/HR/AbstractPerson.cs
+++ Implementation/HR/AbstractPerson.cs
@@ -1,39 +1,27 @@
-﻿namespace Practice.HR
+﻿using System;
+using Practice.Common;
+using Practice.HR.Events;
+
+namespace Practice.HR
 {
     /// <summary>
     ///     Абстрактная база для описания конкретных реализаций типа "Человек".
     ///     Используется для дальнейшего наследования.
     /// </summary>
-    internal abstract class AbstractPerson
+    internal abstract class AbstractPerson : IPerson
     {
-        internal Organization.Department Department
+        private IName _name;
+        public IName Name
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
+            get => _name;
             set
             {
-                throw new System.NotImplementedException();
+                var args = new ValueChangeEventArgs<IName>(_name);
+                _name = value;
+                NameChange?.Invoke(this, args);
             }
         }
 
-        internal Common.Name Name
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-        
-        /*
-         * TODO #3: Реализуйте интерфейс IPerson для класса AbstractPerson
-         */
+        public event EventHandler<ValueChangeEventArgs<IName>> NameChange;
     }
 }
