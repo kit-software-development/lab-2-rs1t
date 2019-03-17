2358fe2716c649be41a97e565a7ea4bcbe197ad4
Dmitriy Drobotov <dmitriy.drobotov@studiomobile.ru>
2019-03-17 15:11:26 +0300

    Implement library interfaces

--- Implementation/Common/Name.cs
+++ Implementation/Common/Name.cs
@@ -1,28 +1,28 @@
-﻿namespace Practice.Common
+﻿using System.Linq;
+
+namespace Practice.Common
 {
     /// <summary>
     ///     Скрытая реализация представления об имени человека.
     /// </summary>
-    internal struct Name
+    internal struct Name : IName
     {
-        /*
-         * TODO #1: Реализуйте интерфейс IName для структуры Name
-         */
-
         /// <summary>
         ///     Имя.
         /// </summary>
-        public string FirstName { get; }
-        
+        public string FirstName { get; set; }
+
         /// <summary>
         ///     Фамилия.
         /// </summary>
-        public string Surname { get; }
-        
+        public string Surname { get; set; }
+
         /// <summary>
         ///     Отчество.
         /// </summary>
-        public string Patronymic { get; }
-        
+        public string Patronymic { get; set; }
+
+        public string FullName => $"{Surname} {FirstName} {Patronymic}";
+        public string ShortName => $"{Surname} {FirstName.First()}. {Patronymic.First()}.";
     }
 }
