65a35fdfef1798529fceb63315c7806b3fb05eeb
Dmitriy Drobotov <dmitriy.drobotov@studiomobile.ru>
2019-03-17 15:13:02 +0300

    Implement client and employee builders

--- Implementation/HR/Builders.cs
+++ Implementation/HR/Builders.cs
@@ -1,4 +1,6 @@
 using System;
+using Practice.Common;
+using Practice.Organization;
 
 namespace Practice.HR
 {
@@ -8,6 +10,9 @@ namespace Practice.HR
     /// </summary>
     public static class Builders
     {
+        private static IClientBuilder _clientBuilder;
+        private static IEmployeeBuilder _employeeBuilder;
+
         /// <summary>
         ///     Возвращает экземпляр "Строителя" клиентов.
         /// </summary>
@@ -16,10 +21,7 @@ namespace Practice.HR
         /// </returns>
         public static IClientBuilder ClientBuilder()
         {
-            /*
-             * TODO #6: Реализовать фабричный метод ClientBuilder класса Builders
-             */
-            throw new NotImplementedException();
+            return _clientBuilder ?? (_clientBuilder = new ClientBuilder());
         }
 
         /// <summary>
@@ -30,11 +32,108 @@ namespace Practice.HR
         /// </returns>
         public static IEmployeeBuilder EmployeeBuilder()
         {
-            /*
-             * TODO #7: Реализовать фабричный метод EmployeeBuilder класса Builders
-             */
-            throw new NotImplementedException();
+            return _employeeBuilder ?? (_employeeBuilder = new EmployeeBuilder());
+        }
+    }
+
+    internal class EmployeeBuilder : IEmployeeBuilder
+    {
+        private IName _name;
+        private IDepartment _department;
+
+        public IEmployee Build()
+        {
+            if (_name == null)
+                throw new ArgumentNullException(nameof(_name), "Name is required");
+
+            var employee = new Employee
+            {
+                Name = _name,
+                Department = _department
+            };
+
+            _name = null;
+            _department = null;
+
+            return employee;
         }
 
+        public IEmployeeBuilder Name(IName name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public IEmployeeBuilder Name(string name, string surname, string patronymic)
+        {
+            return Name(
+                new Name
+                {
+                    FirstName = name,
+                    Surname = surname,
+                    Patronymic = patronymic,
+                });
+        }
+
+        public IEmployeeBuilder Department(IDepartment department)
+        {
+            _department = department;
+            return this;
+        }
+
+        public IEmployeeBuilder Department(string department)
+        {
+            return Department(
+                new Department
+                {
+                    Name = department,
+                });
+        }
+    }
+
+    internal class ClientBuilder : IClientBuilder
+    {
+        private IName _name;
+        private float _discount;
+
+        public IClient Build()
+        {
+            if (_name == null)
+                throw new ArgumentNullException(nameof(_name), "Name is required");
+
+            var client = new Client
+            {
+                Name = _name,
+                Discount = _discount
+            };
+
+            _name = null;
+            _discount = default(float);
+
+            return client;
+        }
+
+        public IClientBuilder Name(IName name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public IClientBuilder Name(string name, string surname, string patronymic)
+        {
+            return Name(
+                new Name
+                {
+                    FirstName = name,
+                    Surname = surname,
+                    Patronymic = patronymic,
+                });
+        }
+
+        public IClientBuilder Discount(float discount)
+        {
+            _discount = discount;
+            return this;
+        }
     }
 }
